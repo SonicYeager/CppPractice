64eb9b53275a6f5e3cd8127806d79f696bc2698a
SonicYeager <kimstone.yeager@gmail.com>
2020-10-26 11:18:16 +0100

    retro

--- Commandline/Commandline_Unittests/test_Commandline.cpp
+++ Commandline/Commandline_Unittests/test_Commandline.cpp
@@ -5,12 +5,12 @@
 class MockEventListener
 {
 public:
-	MOCK_METHOD(void, NumberEventHandler, (const int& num));
-	MOCK_METHOD(void, TextEventHandler, (const std::string& msg));
-	MOCK_METHOD(void, TextListEventHandler, (const std::vector<std::string>& texts));
-	MOCK_METHOD(void, MoneyEventHandler, (const double& mn));
-	MOCK_METHOD(void, HelpEventHandler, ());
-	MOCK_METHOD(void, ErrorEventHandler, (const std::string& err));
+	MOCK_METHOD(void, OnNumber, (const int& num));
+	MOCK_METHOD(void, OnText, (const std::string& msg));
+	MOCK_METHOD(void, OnTextList, (const std::vector<std::string>& texts));
+	MOCK_METHOD(void, OnMoney, (const double& mn));
+	MOCK_METHOD(void, OnHelp, ());
+	MOCK_METHOD(void, OnError, (const std::string& err));
 };
 
 class TestCommandEventTrigger : public ::testing::Test
@@ -21,46 +21,46 @@ protected:
 	::testing::InSequence seq;
 	virtual void SetUp()
 	{
-		cmdEvntTrgr.onNumber = std::bind(&MockEventListener::NumberEventHandler, &evntLst, std::placeholders::_1);
-		cmdEvntTrgr.onText = std::bind(&MockEventListener::TextEventHandler, &evntLst, std::placeholders::_1);
-		cmdEvntTrgr.onTextList = std::bind(&MockEventListener::TextListEventHandler, &evntLst, std::placeholders::_1);
-		cmdEvntTrgr.onMoney = std::bind(&MockEventListener::MoneyEventHandler, &evntLst, std::placeholders::_1);
-		cmdEvntTrgr.onError = std::bind(&MockEventListener::ErrorEventHandler, &evntLst, std::placeholders::_1);
-		cmdEvntTrgr.onHelp = std::bind(&MockEventListener::HelpEventHandler, &evntLst);
+		cmdEvntTrgr.onNumber = std::bind(&MockEventListener::OnNumber, &evntLst, std::placeholders::_1);
+		cmdEvntTrgr.onText = std::bind(&MockEventListener::OnText, &evntLst, std::placeholders::_1);
+		cmdEvntTrgr.onTextList = std::bind(&MockEventListener::OnTextList, &evntLst, std::placeholders::_1);
+		cmdEvntTrgr.onMoney = std::bind(&MockEventListener::OnMoney, &evntLst, std::placeholders::_1);
+		cmdEvntTrgr.onError = std::bind(&MockEventListener::OnError, &evntLst, std::placeholders::_1);
+		cmdEvntTrgr.onHelp = std::bind(&MockEventListener::OnHelp, &evntLst);
 	}
 };
 
 TEST_F(TestCommandEventTrigger, Trigger_HelpArg_HelpEvent)
 {
 	//setup
-	EXPECT_CALL(evntLst, HelpEventHandler());
+	EXPECT_CALL(evntLst, OnHelp());
 
 	//excution
 	cmdEvntTrgr.Trigger({"--h"});
 }
 
-TEST_F(TestCommandEventTrigger, Trigger_Empty_HelpEvent)
+TEST_F(TestCommandEventTrigger, Trigger_NoCommandArgs_CallOnHelp)
 {
 	//setup
-	EXPECT_CALL(evntLst, HelpEventHandler());
+	EXPECT_CALL(evntLst, OnHelp());
 
 	//excution
 	cmdEvntTrgr.Trigger({ });
 }
 
-TEST_F(TestCommandEventTrigger, Trigger_Number5_NumberEventWithNum)
+TEST_F(TestCommandEventTrigger, Trigger_Number5_CallOnNumber)
 {
 	//setup
-	EXPECT_CALL(evntLst, NumberEventHandler(5));
+	EXPECT_CALL(evntLst, OnNumber(5));
 
 	//excution
 	cmdEvntTrgr.Trigger({"--n", "5" });
 }
 
-TEST_F(TestCommandEventTrigger, Trigger_TextHello_TextEvent)
+TEST_F(TestCommandEventTrigger, Trigger_TextHello_CallOnText)
 {
 	//setup
-	EXPECT_CALL(evntLst, TextEventHandler("Hello"));
+	EXPECT_CALL(evntLst, OnText("Hello"));
 
 	//excution
 	cmdEvntTrgr.Trigger({"--t", "Hello" });
@@ -69,7 +69,7 @@ TEST_F(TestCommandEventTrigger, Trigger_TextHello_TextEvent)
 TEST_F(TestCommandEventTrigger, Trigger_TextListHello_TextListEvent)
 {
 	//setup
-	EXPECT_CALL(evntLst, TextListEventHandler(std::vector<std::string>{"Hello", "Hello"}));
+	EXPECT_CALL(evntLst, OnTextList(std::vector<std::string>{"Hello", "Hello"}));
 
 	//excution
 	cmdEvntTrgr.Trigger({"--tl", "Hello", "Hello" });
@@ -78,7 +78,7 @@ TEST_F(TestCommandEventTrigger, Trigger_TextListHello_TextListEvent)
 TEST_F(TestCommandEventTrigger, Trigger_Money4Dot5_MoneyEvent)
 {
 	//setup
-	EXPECT_CALL(evntLst, MoneyEventHandler(4.5));
+	EXPECT_CALL(evntLst, OnMoney(4.5));
 
 	//excution
 	cmdEvntTrgr.Trigger({"--m", "4.5"});
@@ -87,7 +87,7 @@ TEST_F(TestCommandEventTrigger, Trigger_Money4Dot5_MoneyEvent)
 TEST_F(TestCommandEventTrigger, Trigger_Nonsense_ErrorEvent)
 {
 	//setup
-	EXPECT_CALL(evntLst, ErrorEventHandler("--dasgdfsm"));
+	EXPECT_CALL(evntLst, OnError("--dasgdfsm"));
 
 	//excution
 	cmdEvntTrgr.Trigger({ "--dasgdfsm", "lkdfnv-" });
@@ -96,8 +96,8 @@ TEST_F(TestCommandEventTrigger, Trigger_Nonsense_ErrorEvent)
 TEST_F(TestCommandEventTrigger, Trigger_Number5AndNumber6_TwoCalls)
 {
 	//setup
-	EXPECT_CALL(evntLst, NumberEventHandler(5));
-	EXPECT_CALL(evntLst, NumberEventHandler(6));
+	EXPECT_CALL(evntLst, OnNumber(5));
+	EXPECT_CALL(evntLst, OnNumber(6));
 
 	//excution
 	cmdEvntTrgr.Trigger({ "--n", "5", "--n", "6"});
@@ -106,8 +106,8 @@ TEST_F(TestCommandEventTrigger, Trigger_Number5AndNumber6_TwoCalls)
 TEST_F(TestCommandEventTrigger, Trigger_TextHelloAndBye_TwoCalls)
 {
 	//setup
-	EXPECT_CALL(evntLst, TextEventHandler("Hello"));
-	EXPECT_CALL(evntLst, TextEventHandler("Bye"));
+	EXPECT_CALL(evntLst, OnText("Hello"));
+	EXPECT_CALL(evntLst, OnText("Bye"));
 
 	//excution
 	cmdEvntTrgr.Trigger({ "--t", "Hello", "--t", "Bye" });
@@ -116,8 +116,8 @@ TEST_F(TestCommandEventTrigger, Trigger_TextHelloAndBye_TwoCalls)
 TEST_F(TestCommandEventTrigger, Trigger_TextListHelloTwice_TwoCalls)
 {
 	//setup
-	EXPECT_CALL(evntLst, TextListEventHandler(std::vector<std::string>{"Hello", "Hello"}));
-	EXPECT_CALL(evntLst, TextListEventHandler(std::vector<std::string>{"Hello", "Hello"}));
+	EXPECT_CALL(evntLst, OnTextList(std::vector<std::string>{"Hello", "Hello"}));
+	EXPECT_CALL(evntLst, OnTextList(std::vector<std::string>{"Hello", "Hello"}));
 
 	//excution
 	cmdEvntTrgr.Trigger({ "--tl", "Hello", "Hello", "--tl", "Hello", "Hello" });
@@ -126,8 +126,8 @@ TEST_F(TestCommandEventTrigger, Trigger_TextListHelloTwice_TwoCalls)
 TEST_F(TestCommandEventTrigger, Trigger_Money4Dot5And10Dot6_TwoCalls)
 {
 	//setup
-	EXPECT_CALL(evntLst, MoneyEventHandler(4.5));
-	EXPECT_CALL(evntLst, MoneyEventHandler(10.6));
+	EXPECT_CALL(evntLst, OnMoney(4.5));
+	EXPECT_CALL(evntLst, OnMoney(10.6));
 
 	//excution
 	cmdEvntTrgr.Trigger({ "--m", "4.5", "--m", "10.6" });
@@ -136,11 +136,11 @@ TEST_F(TestCommandEventTrigger, Trigger_Money4Dot5And10Dot6_TwoCalls)
 TEST_F(TestCommandEventTrigger, Trigger_AllCmdsOnce_MultipleCalls)
 {
 	//setup
-	EXPECT_CALL(evntLst, ErrorEventHandler("--j"));
-	EXPECT_CALL(evntLst, NumberEventHandler(4));
-	EXPECT_CALL(evntLst, TextEventHandler("Some big Test!"));
-	EXPECT_CALL(evntLst, TextListEventHandler(std::vector<std::string>{"Some", "Big", "Test"}));
-	EXPECT_CALL(evntLst, MoneyEventHandler(5.8));
+	EXPECT_CALL(evntLst, OnError("--j"));
+	EXPECT_CALL(evntLst, OnNumber(4));
+	EXPECT_CALL(evntLst, OnText("Some big Test!"));
+	EXPECT_CALL(evntLst, OnTextList(std::vector<std::string>{"Some", "Big", "Test"}));
+	EXPECT_CALL(evntLst, OnMoney(5.8));
 
 	//excution
 	cmdEvntTrgr.Trigger({"--j", "--n", "4", "--t", "Some big Test!", "--tl", "Some", "Big", "Test", "--m", "5.8" });
