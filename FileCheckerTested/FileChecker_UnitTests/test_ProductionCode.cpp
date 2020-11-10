f0a3e3eeba4b7a381eb77d9283fdadc3ee52901a
SonicYeager <kimstone.yeager@gmail.com>
2020-11-10 15:16:32 +0100

    created stub for tests (StubReader)

--- FileCheckerTested/FileChecker_UnitTests/test_ProductionCode.cpp
+++ FileCheckerTested/FileChecker_UnitTests/test_ProductionCode.cpp
@@ -1,6 +1,20 @@
 #include "gmock/gmock.h"
 #include "ProductCode.h"
 
+class StubReader : public LegacyReader
+{
+public:
+	MOCK_METHOD(void, SetLib, (const std::string&), (override));
+	bool CheckFile(const std::wstring&) override
+	{
+		return true;
+	}
+	bool IsExtensionSupported(const std::wstring&) override
+	{
+		return true;
+	}
+};
+
 //TEST(TestFileChecker, Check_ValidPath_ReturnTrue)
 //{
 //	FileChecker fc;
