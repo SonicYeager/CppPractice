d4fa42073bec1d6aa471e0250b47381d9dc83cc3
SonicYeager <kimstone.yeager@gmail.com>
2020-11-11 15:23:00 +0100

    test OneElem -> correct index; Multiple Elem -> correct index

--- InMemoryDirectory_UnitTests/test_InMemoryDirectory.cpp
+++ InMemoryDirectory_UnitTests/test_InMemoryDirectory.cpp
@@ -12,3 +12,32 @@ TEST(TestInMemoryDirectory, AddElement_AddOneElement_OneElementAndOneIndexWithTe
 	auto expected = Element("Heinz");
 	EXPECT_EQ(*actual, expected);
 }
+
+TEST(TestInMemoryDirectory, GenerateIndex_AddOneElement_OneIndexWithCorrectText)
+{
+	InMemoryDirectory imd{};
+
+	imd.AddElement(std::make_unique<Element>("Heinz"));
+	imd.GenerateIndex();
+
+	auto index = imd.GetElement("index");
+	auto actual = index->GetText();
+	auto expected = "Index\nHeinz\n";
+	EXPECT_EQ(actual, expected);
+}
+
+TEST(TestInMemoryDirectory, GenerateIndex_AddMultipleElement_OneIndexWithCorrectText)
+{
+	InMemoryDirectory imd{};
+
+	imd.AddElement(std::make_unique<Element>("Heinz"));
+	imd.AddElement(std::make_unique<Element>("Kunz"));
+	imd.AddElement(std::make_unique<Element>("Donald"));
+	imd.AddElement(std::make_unique<Element>("Biden"));
+	imd.GenerateIndex();
+
+	auto index = imd.GetElement("index");
+	auto actual = index->GetText();
+	auto expected = "Index\nHeinz\nKunz\nDonald\nBiden\n";
+	EXPECT_EQ(actual, expected);
+}
