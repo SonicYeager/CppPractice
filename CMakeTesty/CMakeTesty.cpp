43366d71139e162f4786c52a6b2027a41e26d92f
SonicYeager <kimstone.yeager@gmail.com>
2020-11-12 11:05:47 +0100

    CMakeTesty

---
+++ CMakeTesty/CMakeTesty.cpp
@@ -0,0 +1,17 @@
+﻿// CMakeTesty.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "CMakeTesty.h"
+#include "Config.h"
+
+int main(int argc, char* argv[])
+{
+	if (argc < 2) {
+		// report version
+		std::cout << argv[0] << " Version " << CMakeTesty_VERSION_MAJOR << "."
+			<< CMakeTesty_VERSION_MINOR << std::endl;
+		std::cout << "Usage: " << argv[0] << " number" << std::endl;
+		return 1;
+	}
+	return 0;
+}
---
+++ CMakeTesty/out/build/x64-Debug (Standard)/Config.h
@@ -0,0 +1,3 @@
+// the configured options and settings for Tutorial
+#define CMakeTesty_VERSION_MAJOR 0
+#define CMakeTesty_VERSION_MINOR 1
