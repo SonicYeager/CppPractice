a10cd0012d852d8aba624bd72c4c0e3294104c3e
SonicYeager <kimstone.yeager@gmail.com>
2020-11-12 09:18:51 +0100

    tests for GenerateIndex

--- InMemoryDirectory_UnitTests/test_InMemoryDirectory.cpp
+++ InMemoryDirectory_UnitTests/test_InMemoryDirectory.cpp
@@ -7,6 +7,7 @@ bool operator==(const Element& left, const Element& right)
 	return left.GetName() == right.GetName() && left.GetText() == right.GetText();
 }
 
+
 TEST(TestInMemoryDirectory, AddElement_AddOne_Heinz)
 {
 	InMemoryDirectory imd{};
@@ -43,5 +44,53 @@ TEST(TestInMemoryDirectory, AddElement_AddMultipleWithGenerateIndexBefore_Update
 	auto expected = "Heinz\nKarl\nGustav\n";
 	EXPECT_EQ(actual, expected);
 	auto actualCount = imd.GetElementCount();
-	EXPECT_EQ(actual, 4);
+	EXPECT_EQ(actualCount, 4);
+}
+
+TEST(TestInMemoryDirectory, GenerateIndex_OneTimes_CorrectIndex)
+{
+	InMemoryDirectory imd{};
+
+	imd.AddElement(std::make_unique<Element>("Fury"));
+	imd.AddElement(std::make_unique<Element>("Tony"));
+	imd.AddElement(std::make_unique<Element>("Bruce"));
+	imd.GenerateIndex();
+
+	auto actual = imd.GetElement("index")->GetText();
+	auto expected = "Fury\nTony\nBruce\n";
+	EXPECT_EQ(actual, expected);
+	auto actualCount = imd.GetElementCount();
+	EXPECT_EQ(actualCount, 4);
+}
+
+TEST(TestInMemoryDirectory, GenerateIndex_MultipleTimesWithAddBetween_JustOneIndex)
+{
+	InMemoryDirectory imd{};
+
+	imd.GenerateIndex();
+	imd.AddElement(std::make_unique<Element>("Heinz"));
+	imd.AddElement(std::make_unique<Element>("Knut"));
+	imd.AddElement(std::make_unique<Element>("Chester"));
+	imd.GenerateIndex();
+
+	auto actual = imd.GetElement("index")->GetText();
+	auto expected = "Heinz\nKnut\nChester\n";
+	EXPECT_EQ(actual, expected);
+	auto actualCount = imd.GetElementCount();
+	EXPECT_EQ(actualCount, 4);
+}
+
+TEST(TestInMemoryDirectory, GenerateIndex_MultipleTimes_JustOneIndex)
+{
+	InMemoryDirectory imd{};
+
+	imd.AddElement(std::make_unique<Element>("Heinz"));
+	imd.GenerateIndex();
+	imd.GenerateIndex();
+
+	auto actual = imd.GetElement("index")->GetText();
+	auto expected = "Heinz\n";
+	EXPECT_EQ(actual, expected);
+	auto actualCount = imd.GetElementCount();
+	EXPECT_EQ(actualCount, 2);
 }
