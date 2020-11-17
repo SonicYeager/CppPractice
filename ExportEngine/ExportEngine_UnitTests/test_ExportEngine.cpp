3f7582eba895a6132eede55bb20a59a1528019f8
SonicYeager <kimstone.yeager@gmail.com>
2020-11-17 10:38:17 +0100

    prep refac;

--- ExportEngine/ExportEngine_UnitTests/test_ExportEngine.cpp
+++ ExportEngine/ExportEngine_UnitTests/test_ExportEngine.cpp
@@ -1,9 +1,6 @@
 #include "gmock/gmock.h"
 #include "ExportEngine.h"
-
-
-
-TEST(TestExportEngine, Bounce_MainTest_ReturnTrue)
+TEST(TestExportEngine, Bounce_WithFlagsBounceIfValidAndRenameFilenameIfExists_ReturnTrue)
 {
 	ExportEngine exporter{};
 	ProjectInfo PI{};
@@ -14,6 +11,48 @@ TEST(TestExportEngine, Bounce_MainTest_ReturnTrue)
 	ExportEngineConfig config{};
 	config.pPI = &PI;
 	config.flagsExport = BOUNCE_IF_VALID | RENAME_FILENAME_IF_EXIST; // | RGB_EXPORT;
+	config.pUserInterface = IUserInterface::Create();
+
+	bool actual = exporter.Bounce(config);
+
+	EXPECT_TRUE(actual);
+
+	//cleanup
+	delete config.pUserInterface;
+}
+
+TEST(TestExportEngine, Bounce_WithFlagsBounceIfValidAndRenameFilenameIfExistsAndRGBExport_ReturnTrue)
+{
+	ExportEngine exporter{};
+	ProjectInfo PI{};
+	PI.rangeEnd = 200;
+	PI.width = 12;
+	PI.height = 12;
+	PI.aspectRation = 1.0;
+	ExportEngineConfig config{};
+	config.pPI = &PI;
+	config.flagsExport = BOUNCE_IF_VALID | RENAME_FILENAME_IF_EXIST | RGB_EXPORT;
+	config.pUserInterface = IUserInterface::Create();
+
+	bool actual = exporter.Bounce(config);
+
+	EXPECT_TRUE(actual);
+
+	//cleanup
+	delete config.pUserInterface;
+}
+
+TEST(TestExportEngine, Bounce_WithFlagsBounceIfValidAndAndRGBExport_ReturnTrue)
+{
+	ExportEngine exporter{};
+	ProjectInfo PI{};
+	PI.rangeEnd = 200;
+	PI.width = 12;
+	PI.height = 12;
+	PI.aspectRation = 1.0;
+	ExportEngineConfig config{};
+	config.pPI = &PI;
+	config.flagsExport = BOUNCE_IF_VALID | RGB_EXPORT;
 	config.pUserInterface = IUserInterface::Create();
 
 	bool actual = exporter.Bounce(config);
