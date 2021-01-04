3d905172124e12a089429ac88f3def38da1ff1a8
SonicYeager <kimstone.yeager@gmail.com>
2021-01-04 09:09:13 +0100

    extracted GetOpenedProgress;

--- ExportEngine/ExportEngine.cpp
+++ ExportEngine/ExportEngine.cpp
@@ -10,41 +10,52 @@
 #include "LogHandler.h"
 #include "ExportHandler.h"
 
+Progress GetOpenedProgress(IUserInterface* ui, long long range)
+{
+	Progress progress{ui};
+	progress.OpenProgress(range);
+}
+
 int ExportIfBounceIsValid(const ExportEngineConfig& config)
 {
 	int result{-1};
-	ExportHandler expHandler{config.pExporter, config.createExport, static_cast<ExportFlags>(config.flagsExport)};
-	if(expHandler.CheckBounceIsValid(config))
+	try
 	{
-		ThrowIfProtectedFeature(expHandler.GetExportConfig().type);
-		Progress progress{config.pUserInterface};
-		progress.OpenProgress(config.pPI->rangeEnd - config.pPI->rangeStart);
-		auto targetPath = ConfigDirectory(static_cast<ExportFlags>(config.flagsExport) == ExportFlags::RENAME_FILENAME_IF_EXIST, config.targetFileName);
-		WrappedVideoEngine wVideoEng;
-		wVideoEng.Prepare(*config.pPI);
-		expHandler.Initialize(targetPath);
-		LogExportRange(config.pPI->rangeStart, config.pPI->rangeEnd, targetPath.string());
-		Measurement measurement;
-		measurement.Start();
-		expHandler.ExportFrames(progress, result, wVideoEng, config);
-		measurement.Stop();
-		LogExportTime(config.pPI->rangeEnd - config.pPI->rangeStart / config.pPI->frameRate, measurement.GetElapsedTime());
-		result = 1;
+		ExportHandler expHandler{config.pExporter, config.createExport, static_cast<ExportFlags>(config.flagsExport)};
+		if(expHandler.CheckBounceIsValid(config))
+		{
+			ThrowIfProtectedFeature(expHandler.GetExportConfig().type);
+			auto progress = GetOpenedProgress(config.pUserInterface, config.pPI->rangeEnd - config.pPI->rangeStart);
+
+			auto targetPath = ConfigDirectory(static_cast<ExportFlags>(config.flagsExport) == ExportFlags::RENAME_FILENAME_IF_EXIST, config.targetFileName);
+
+			WrappedVideoEngine wVideoEng;
+			wVideoEng.Prepare(*config.pPI);
+
+			expHandler.Initialize(targetPath);
+
+			LogExportRange(config.pPI->rangeStart, config.pPI->rangeEnd, targetPath.string());
+
+			Measurement measurement;
+
+			measurement.Start();
+			expHandler.ExportFrames(progress, result, wVideoEng, config);
+			measurement.Stop();
+
+			LogExportTime(config.pPI->rangeEnd - config.pPI->rangeStart / config.pPI->frameRate, measurement.GetElapsedTime());
+			result = 1;
+		}
+	}
+	catch(std::exception ex)
+	{
+		LogExportError(ex);
 	}
 	return result;
 }
 
 bool ExportEngine::Bounce(const ExportEngineConfig& config)
 {
-	int result{-1};
-	try
-	{
-		result = ExportIfBounceIsValid(config);
-	}
-	catch(std::exception ex)
-	{
-		LogExportError(ex);
-	}
+	int result = ExportIfBounceIsValid(config);
 	return result == 1;
 }
 
