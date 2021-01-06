558f219c1a927a65b16f2d9c51123680952b98d3
Magnus Cook <kimstone.yeager@gmail.com>
2021-01-06 14:36:02 +0100

    mutliple tc with/without overtime rate 9;

--- Payroll/PayrollSystem_UnitTests/test_HourlyClassification.cpp
+++ Payroll/PayrollSystem_UnitTests/test_HourlyClassification.cpp
@@ -26,3 +26,31 @@ TEST(TestHourlyClassification, CalculatePay_Rate9OneTimeCard9HOvertime_Return85k
 
 	EXPECT_EQ(actual, expected);
 }
+
+TEST(TestHourlyClassification, CalculatePay_Rate9MultipleTimeCard5HNoOvertime_Return90)
+{
+	Payroll::HourlyClassification hc{ 9.0 };
+	Payroll::Paycheck pc{ {1, 2, 2000 }, {4, 2, 2000 } };
+	Payroll::TimeCard tc{ {2, 2, 2000 }, 5.0 };
+	Payroll::TimeCard tc{ {3, 2, 2000 }, 5.0 };
+	hc.AddTimeCard(tc);
+
+	auto actual = hc.CalculatePay(pc);
+	double expected = 90.0;
+
+	EXPECT_EQ(actual, expected);
+}
+
+TEST(TestHourlyClassification, CalculatePay_Rate9MultipleTimeCard9HOvertime_Return171)
+{
+	Payroll::HourlyClassification hc{ 9.0 };
+	Payroll::Paycheck pc{ {1, 2, 2000 }, {4, 2, 2000 } };
+	Payroll::TimeCard tc{ {2, 2, 2000 }, 9.0 };
+	Payroll::TimeCard tc{ {3, 2, 2000 }, 9.0 };
+	hc.AddTimeCard(tc);
+
+	auto actual = hc.CalculatePay(pc);
+	double expected = 171.0;
+
+	EXPECT_EQ(actual, expected);
+}
