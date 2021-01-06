10a897b0577eca5c3796d51f7e9dc1d1d8068b25
Magnus Cook <kimstone.yeager@gmail.com>
2021-01-06 13:34:30 +0100

    year 2000 above 3640;

--- Payroll/PayrollSystem_UnitTests/test_GermanExpenses.cpp
+++ Payroll/PayrollSystem_UnitTests/test_GermanExpenses.cpp
@@ -12,3 +12,15 @@ TEST(TestGermanExpenses, CalculateDeductions_2500Year2000_Return)
 
 	EXPECT_EQ(actual, 830.0);
 }
+
+TEST(TestGermanExpenses, CalculateDeductions_4000Year2000_Return1400)
+{
+	Payroll::GermanExpenses gexp{};
+	Payroll::Paycheck pc{};
+	pc.m_grossPay = 4000.0;
+	pc.m_payPeriodStart = Date(1, 1, 2000);
+
+	auto actual = gexp.CalculateDeductions(pc);
+
+	EXPECT_EQ(actual, 1400.0);
+}
