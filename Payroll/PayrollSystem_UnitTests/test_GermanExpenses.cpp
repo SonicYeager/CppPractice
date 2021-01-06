f2e3935db8ac6c26b3e1210510f981e235177dd4
Magnus Cook <kimstone.yeager@gmail.com>
2021-01-06 13:42:22 +0100

    year 2006 under and above 10001;

--- Payroll/PayrollSystem_UnitTests/test_GermanExpenses.cpp
+++ Payroll/PayrollSystem_UnitTests/test_GermanExpenses.cpp
@@ -1,7 +1,7 @@
 #include "gmock/gmock.h"
 #include "../PayrollSystem/GermanExpenses.h"
 
-TEST(TestGermanExpenses, CalculateDeductions_2500Year2000_Return)
+TEST(TestGermanExpenses, CalculateDeductions_2500Year2000_Return830)
 {
 	Payroll::GermanExpenses gexp{};
 	Payroll::Paycheck pc{};
@@ -23,4 +23,28 @@ TEST(TestGermanExpenses, CalculateDeductions_4000Year2000_Return1400)
 	auto actual = gexp.CalculateDeductions(pc);
 
 	EXPECT_EQ(actual, 1400.0);
+}
+
+TEST(TestGermanExpenses, CalculateDeductions_2500Year2006_Return830)
+{
+	Payroll::GermanExpenses gexp{};
+	Payroll::Paycheck pc{};
+	pc.m_grossPay = 2500.0;
+	pc.m_payPeriodStart = Date(1, 1, 2006);
+
+	auto actual = gexp.CalculateDeductions(pc);
+
+	EXPECT_EQ(actual, 830.0);
+}
+
+TEST(TestGermanExpenses, CalculateDeductions_11000Year2006_Return4035dot33)
+{
+	Payroll::GermanExpenses gexp{};
+	Payroll::Paycheck pc{};
+	pc.m_grossPay = 11000.0;
+	pc.m_payPeriodStart = Date(1, 1, 2006);
+
+	auto actual = gexp.CalculateDeductions(pc);
+
+	EXPECT_EQ(actual, 4035.33);
 }
