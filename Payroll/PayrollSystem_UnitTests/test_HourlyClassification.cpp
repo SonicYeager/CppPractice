8a409d25efca98cf9c16b03c84d346aa9f0ebd24
Magnus Cook <kimstone.yeager@gmail.com>
2021-01-06 14:32:05 +0100

    casual rate and worktime (single wortimecard);

--- Payroll/PayrollSystem_UnitTests/test_HourlyClassification.cpp
+++ Payroll/PayrollSystem_UnitTests/test_HourlyClassification.cpp
@@ -1,3 +1,28 @@
 #include "gmock/gmock.h"
 #include "../PayrollSystem/HourlyClassification.h"
 
+TEST(TestHourlyClassification, CalculatePay_Rate9OneTimeCard5HNoOvertime_Return45)
+{
+	Payroll::HourlyClassification hc{9.0};
+	Payroll::Paycheck pc{ {1, 2, 2000 }, {3, 2, 2000 } };
+	Payroll::TimeCard tc{ {2, 2, 2000 }, 5.0 };
+	hc.AddTimeCard(tc);
+
+	auto actual = hc.CalculatePay(pc);
+	double expected = 45.0;
+
+	EXPECT_EQ(actual, expected);
+}
+
+TEST(TestHourlyClassification, CalculatePay_Rate9OneTimeCard9HOvertime_Return85k5)
+{
+	Payroll::HourlyClassification hc{ 9.0 };
+	Payroll::Paycheck pc{ {1, 2, 2000 }, {3, 2, 2000 } };
+	Payroll::TimeCard tc{ {2, 2, 2000 }, 9.0 };
+	hc.AddTimeCard(tc);
+
+	auto actual = hc.CalculatePay(pc);
+	double expected = 85.5;
+
+	EXPECT_EQ(actual, expected);
+}
