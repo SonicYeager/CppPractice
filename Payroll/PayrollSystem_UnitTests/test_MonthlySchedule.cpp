7cf24063613683d6a9ebf3ac79cf37d963b61cdb
Magnus Cook <kimstone.yeager@gmail.com>
2021-01-07 06:08:15 +0100

    PayDay on leap year;

--- Payroll/PayrollSystem_UnitTests/test_MonthlySchedule.cpp
+++ Payroll/PayrollSystem_UnitTests/test_MonthlySchedule.cpp
@@ -10,11 +10,29 @@ TEST(TestMonthlySchedule, IsPayDay_PayDay_ReturnTrue)
 	EXPECT_TRUE(actual);
 }
 
-TEST(TestMonthlySchedule, IsPayDay_NoPayDay_ReturnTrue)
+TEST(TestMonthlySchedule, IsPayDay_NoPayDay_ReturnFalse)
 {
 	Payroll::MonthlySchedule ms{};
 
 	auto actual = ms.IsPayDate({ 30,1,2021 });
+
+	EXPECT_FALSE(actual);
+}
+
+TEST(TestMonthlySchedule, IsPayDay_PayDayOnLeapYear_ReturnTrue)
+{
+	Payroll::MonthlySchedule ms{};
+
+	auto actual = ms.IsPayDate({ 28,2,2020 });
+
+	EXPECT_TRUE(actual);
+}
+
+TEST(TestMonthlySchedule, IsPayDay_NoPayDayOnLeapYear_ReturnFalse)
+{
+	Payroll::MonthlySchedule ms{};
+
+	auto actual = ms.IsPayDate({ 29,2,2020 });
 
 	EXPECT_FALSE(actual);
 }
