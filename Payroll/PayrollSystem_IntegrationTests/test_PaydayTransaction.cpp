4f941c3818bdc0d81115d60c2219f1c6de80dfc0
Magnus Cook <kimstone.yeager@gmail.com>
2021-01-08 12:00:35 +0100

    intr with multiple tc one day;

--- Payroll/PayrollSystem_IntegrationTests/test_PaydayTransaction.cpp
+++ Payroll/PayrollSystem_IntegrationTests/test_PaydayTransaction.cpp
@@ -23,7 +23,7 @@ namespace Payroll
 	{
 		if (left.size() == right.size())
 		{
-			for (auto entry : left)
+			for (const auto& entry : left)
 			{
 				auto correspondingVal = right.find(entry.first);
 				if (not (entry.second == correspondingVal->second))
@@ -37,16 +37,16 @@ namespace Payroll
 
 	std::ostream& operator<<(std::ostream& os, const Date& d)
 	{
-		return os << "{" << d.Day() << ", " << d.Month() << ", " << d.Year() << "}";
+		return os << "{ D: " << d.Day() << ", M: " << d.Month() << ", Y: " << d.Year() << "}";
 	}
 
 	std::ostream& operator<<(std::ostream& os, const Paycheck& pc)
 	{
 		return os
-			<< "{" << pc.m_payPeriodStart << ", "
-			<< pc.m_payPeriodEnd << ", "
-			<< pc.m_grossPay << ", "
-			<< pc.m_deductions << ", "
+			<< "{ START: " << pc.m_payPeriodStart << ", END: "
+			<< pc.m_payPeriodEnd << ", GROSS: "
+			<< pc.m_grossPay << ", DEDUCTION: "
+			<< pc.m_deductions << ", NET: "
 			<< pc.m_netPay << "}";
 	}
 
@@ -54,7 +54,7 @@ namespace Payroll
 	{
 		for(const auto& entry : pcs)
 		{ 
-			os << "{" << entry.first << ", " << entry.second << "}";
+			os << "{ ID: " << entry.first << ", DATA:" << entry.second << "}";
 		}
 		return os;
 	}
@@ -78,12 +78,41 @@ void FillDatabase(Payroll::Database& db)
 	db.AddEmployee(2, std::move(e2));
 }
 
+void FillDatabaseWithMultipleTimeCardsOnOneDay(Payroll::Database& db)
+{
+	std::unique_ptr<Payroll::Employee> e1 = std::make_unique<Payroll::Employee>(1, "V");
+	Payroll::HourlyClassification hc{ 10.0 };
+	hc.AddTimeCard({ {2,1,2021}, 2.0 });
+	hc.AddTimeCard({ {2,1,2021}, 2.0 });
+	e1->SetClassification(std::make_unique<Payroll::HourlyClassification>(hc));
+	e1->SetSchedule(std::make_unique<Payroll::WeeklySchedule>());
+	e1->SetExpenses(std::make_unique<Payroll::CommonExpenses>());
+
+	std::unique_ptr<Payroll::Employee> e2 = std::make_unique<Payroll::Employee>(2, "Data");
+	Payroll::SalariedClassification sc{ 2000.0 };
+	e2->SetClassification(std::make_unique<Payroll::SalariedClassification>(sc));
+	e2->SetSchedule(std::make_unique<Payroll::MonthlySchedule>());
+	e2->SetExpenses(std::make_unique<Payroll::GermanExpenses>());
+	db.AddEmployee(1, std::move(e1));
+	db.AddEmployee(2, std::move(e2));
+}
+
 Payroll::Paychecks GetExpectedPaychecksHourlyEmployee()
 {
 	Payroll::Paycheck pc1{ {4,1,2021}, {8,1,2021} };
 	pc1.m_grossPay = 20.0;
-	pc1.m_netPay = 12.0;
+	pc1.m_netPay = 14.0;
 	pc1.m_deductions = 6.0;
+	Payroll::Paychecks pcs{ {1,{pc1}} };
+	return pcs;
+}
+
+Payroll::Paychecks GetExpectedPaychecksHourlyEmployeeMultiTC()
+{
+	Payroll::Paycheck pc1{ {4,1,2021}, {8,1,2021} };
+	pc1.m_grossPay = 40.0;
+	pc1.m_netPay = 28.0;
+	pc1.m_deductions = 12;
 	Payroll::Paychecks pcs{ {1,{pc1}} };
 	return pcs;
 }
@@ -110,5 +139,35 @@ TEST(TestPaydayTransaction, Execute_PaydayOnOneHouerlyEmployeeCommonExpensesWeek
 	pdtrans.Execute();
 
 	auto epcs = GetExpectedPaychecksHourlyEmployee();
+	EXPECT_EQ(pcs, epcs);
+}
+
+TEST(TestPaydayTransaction, Execute_PaydayOnOneSalariedEmployeeGermanExpensesMonthlySchedule_PaychecksFilledWithOnePaycheck)
+{
+
+	Payroll::Database db{};
+	Payroll::Paychecks pcs{};
+	Date d{ 31,1,2021 };
+	FillDatabase(db);
+	Payroll::PaydayTransaction pdtrans{ &pcs, d, &db };
+
+	pdtrans.Execute();
+
+	auto epcs = GetExpectedPaychecksSalariedEmployee();
+	EXPECT_EQ(pcs, epcs);
+}
+
+TEST(TestPaydayTransaction, Execute_PaydayOnOneHouerlyEmployeeCommonExpensesWeeklyScheduleMultiTCOneDay_PaychecksFilledWithOnePaycheck)
+{
+
+	Payroll::Database db{};
+	Payroll::Paychecks pcs{};
+	Date d{ 8,1,2021 };
+	FillDatabaseWithMultipleTimeCardsOnOneDay(db);
+	Payroll::PaydayTransaction pdtrans{ &pcs, d, &db };
+
+	pdtrans.Execute();
+
+	auto epcs = GetExpectedPaychecksHourlyEmployeeMultiTC();
 	EXPECT_EQ(pcs, epcs);
 }
